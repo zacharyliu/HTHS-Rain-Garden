ff1a442120607f99884ed57abdc9fd1ccf688e81
Zachary Liu <liu.zachary@gmail.com>
2011-07-02 10:05:21 -0400

    fixed title

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@ $this->load->helper('url');
 ?><!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $name; ?>HTHS Rain Garden</title>
+	<title><?php echo $name; ?> - HTHS Rain Garden</title>
 	<link href="<?php echo base_url(); ?>css/main.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-1.6.2.js"></script>
 </head>
