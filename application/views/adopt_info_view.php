0f1f9dbf880bd1744b6d282ab3fe44ba1fed6f11
Zachary Liu <liu.zachary@gmail.com>
2011-07-04 01:10:38 -0400

    styled adopt page info box

--- application/views/adopt_info_view.php
+++ application/views/adopt_info_view.php
@@ -1,5 +1,32 @@
-<div class="adopt_info">
-<?php
-$this->load->view('plant_info_view', $info);
-?>
+<style type="text/css">
+#adopt_info {
+    width: 700px;
+    height: 500px;
+}
+#plant_info {
+    float: left;
+    width: 310px;
+    padding: 10px;
+    overflow: auto;
+    height: 480px;
+    border: 1px solid black;
+    box-shadow: 5px 5px 5px #CCC;
+    margin-right: 20px;
+}
+#adopt_box {
+    padding: 10px;
+    overflow: auto;
+    height: 480px;
+    position: relative;
+}
+</style>
+<div id="adopt_info">
+    <div id="plant_info">
+        <?php
+        $this->load->view('plant_info_view', $info);
+        ?>
+    </div>
+    <div id="adopt_box">
+        Adopter:
+    </div>
 </div>
