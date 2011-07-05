e06381671129708687f30eab98214cdafeff78b5
Zachary Liu <liu.zachary@gmail.com>
2011-07-04 23:01:16 -0400

    adopt info formatting

--- application/views/adopt_info_view.php
+++ application/views/adopt_info_view.php
@@ -13,6 +13,23 @@
     box-shadow: 5px 5px 5px #CCC;
     margin-right: 20px;
 }
+#plant_info div {
+    margin-bottom: 0.5em;
+}
+#plant_info div:last-child {
+    margin-bottom: 0;
+}
+
+.plant_image {
+    text-align: center;
+}
+
+.plant_title {
+    font-size: 1.1em;
+    text-align: center;
+    font-family: 'Georgia', serif;
+}
+
 #adopt_box {
     padding: 10px;
     overflow: auto;
