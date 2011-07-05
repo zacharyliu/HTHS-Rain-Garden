97185019110f2d10101837a58310e10f38efef50
Zachary Liu <liu.zachary@gmail.com>
2011-07-04 23:29:02 -0400

    more plant info view style to its own file

--- application/views/adopt_info_view.php
+++ application/views/adopt_info_view.php
@@ -3,33 +3,6 @@
     width: 700px;
     height: 500px;
 }
-#plant_info {
-    float: left;
-    width: 310px;
-    padding: 10px;
-    overflow: auto;
-    height: 480px;
-    border: 1px solid black;
-    box-shadow: 5px 5px 5px #CCC;
-    margin-right: 20px;
-}
-#plant_info div {
-    margin-bottom: 0.5em;
-}
-#plant_info div:last-child {
-    margin-bottom: 0;
-}
-
-.plant_image {
-    text-align: center;
-}
-
-.plant_title {
-    font-size: 1.1em;
-    text-align: center;
-    font-family: 'Georgia', serif;
-}
-
 #adopt_box {
     padding: 10px;
     overflow: auto;
@@ -37,6 +10,7 @@
     position: relative;
 }
 </style>
+<?php $this->load->view('plant_info_view_style'); ?>
 <div id="adopt_info">
     <div id="plant_info">
         <?php
---
+++ application/views/plant_info_view_style.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+#plant_info {
+    float: left;
+    width: 310px;
+    padding: 10px;
+    overflow: auto;
+    height: 480px;
+    border: 1px solid black;
+    box-shadow: 5px 5px 5px #CCC;
+    margin-right: 20px;
+}
+#plant_info div {
+    margin-bottom: 0.5em;
+}
+#plant_info div:last-child {
+    margin-bottom: 0;
+}
+
+.plant_image {
+    text-align: center;
+}
+
+.plant_title {
+    font-size: 1.1em;
+    text-align: center;
+    font-family: 'Georgia', serif;
+}
+</style>
