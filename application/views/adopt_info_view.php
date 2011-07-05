676dffcd1191b3e8340031be6f9fc1d7c69052bf
Zachary Liu <liu.zachary@gmail.com>
2011-07-04 23:34:12 -0400

    fixed styles

--- application/views/adopt_info_view.php
+++ application/views/adopt_info_view.php
@@ -10,34 +10,17 @@
     position: relative;
 }
 
-#plant_info {
+.plant_item {
     float: left;
-    width: 310px;
-    padding: 10px;
-    overflow: auto;
-    height: 480px;
-    border: 1px solid black;
-    box-shadow: 5px 5px 5px #CCC;
     margin-right: 20px;
-}
-#plant_info div {
-    margin-bottom: 0.5em;
-}
-#plant_info div:last-child {
-    margin-bottom: 0;
-}
-
-.plant_image {
-    text-align: center;
+    height: 500px;
 }
 </style>
 <?php $this->load->view('plant_info_view_style'); ?>
 <div id="adopt_info">
-    <div id="plant_info">
-        <?php
+    <?php
         $this->load->view('plant_info_view', $info);
-        ?>
-    </div>
+    ?>
     <div id="adopt_box">
         <?php echo $adopt_box; ?>
     </div>
