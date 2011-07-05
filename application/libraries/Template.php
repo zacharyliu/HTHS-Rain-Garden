7de39f23ea75b4cd9fa4c5fb4761b3ddacaa644a
Zachary Liu <liu.zachary@gmail.com>
2011-07-04 23:06:11 -0400

    create template library

---
+++ application/libraries/Template.php
@@ -0,0 +1,20 @@
+<?php
+
+class Template {
+    
+    public function __construct() {
+        $CI =& get_instance();
+        parent::__construct();
+    }
+    
+    public function render($name, $content) {
+        $data['name'] = $name;
+        $data['content'] = $content;
+        
+        $CI->load->model('Pages_model');
+        $data['list'] = $CI->Pages_model->get_list();
+        
+        $CI->load->view('template', $data);
+    }
+    
+}
